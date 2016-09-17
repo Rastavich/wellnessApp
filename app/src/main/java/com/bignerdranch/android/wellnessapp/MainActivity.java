2b7aac6eeafee29e42d2bc3fd1e022d1e11974af
Isaac Hauser <Isaac Hauser>
2016-09-17 21:38:44 +1000

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/wellnessapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.bignerdranch.android.wellnessapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.hardware.Sensor;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_in);
+    }
+}
