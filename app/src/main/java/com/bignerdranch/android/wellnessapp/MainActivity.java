e60b1139bf883fd2017cdb6a9adb35f88d772830
Isaac <isaac.hauser@live.com.au>
2016-09-21 19:28:09 +1000

    Working on the layout for the login/landing page

--- app/src/main/java/com/bignerdranch/android/wellnessapp/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/wellnessapp/MainActivity.java
@@ -1,14 +1,38 @@
 package com.bignerdranch.android.wellnessapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.hardware.Sensor;
+import android.view.View;
+
+import com.facebook.AccessToken;
+import com.facebook.login.LoginManager;
+
+import static com.facebook.AccessToken.getCurrentAccessToken;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.sign_in);
+        setContentView(R.layout.activity_login);
+
+        if (AccessToken.getCurrentAccessToken() == null) {
+            goLoginScreen();
+        }
     }
+
+    private void goLoginScreen() {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+    public void logout(View view) {
+        LoginManager.getInstance().logOut();
+        goLoginScreen();
+    }
+
+
 }
